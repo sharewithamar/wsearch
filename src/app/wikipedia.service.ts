d9ae6c3bbaa169ffafb839dac819e9d1bea70139
Amarnath <sharewithamar@gmail.com>
2020-03-28 18:48:21 +0530

    rx js pluck - http get - genereic fucntion - Interface typing to prevent errors.

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,5 +1,16 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { pluck } from "rxjs/operators";
+
+interface WikipediaResponse {
+  query: {
+    search: {
+      pageid: number;
+      title: string;
+      snipper: string;
+    }[];
+  };
+}
 
 @Injectable({
   providedIn: "root"
@@ -9,15 +20,17 @@ export class WikipediaService {
   constructor(private http: HttpClient) {}
 
   public search(term: string) {
-    return this.http.get("https://en.wikipedia.org/w/api.php", {
-      params: {
-        action: "query",
-        format: "json",
-        list: "search",
-        utf8: "1",
-        srsearch: term,
-        origin: "*"
-      }
-    });
+    return this.http
+      .get<WikipediaResponse>("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          format: "json",
+          list: "search",
+          utf8: "1",
+          srsearch: term,
+          origin: "*"
+        }
+      })
+      .pipe(pluck("query", "search"));
   }
 }
